f40008106b6db32690e079bef0712f78b1dfa8c5
Vijay <rebalvijay930@gmail.com>
2025-03-14 16:35:55 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const app = express();
+
+
+app.use('/hello',(req,res)=>{
+    res.send("Hello from the Dashboard!.")
+})
+
+
+app.use('/test',(req,res)=>{
+    res.send("Hello from the server!.")
+})
+
+app.listen(7777,()=>{
+    console.log("Server is successfully listening on port 7777...")
+})
