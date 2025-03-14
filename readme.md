96e2a11ebc74a7337357589bcd2b3abf2b806e66
Vijay <rebalvijay930@gmail.com>
2025-03-14 17:04:49 +0530

    exploring get,post,delete

--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
- 
+ app.get -> This will only handle GET call to/test 
+ app.use -> this will match all the http method API calls  to /test
--- src/app.js
+++ src/app.js
@@ -2,11 +2,23 @@ const express = require('express')
 
 const app = express();
 
+app.get("/user",(req,res)=>{
+    res.send({firstName: "Vijya",lastName:"Saini"})
+})
+
+app.post("/user",async(req,res)=>{
+    console.log(req.body)
+    // saving data to DB
+    res.send("Data successfullly saved to the database")
+});
+
+app.delete("/user",(req,res)=>{
+    res.send("Deleted Successfully")
+})
 
 app.use('/hello',(req,res)=>{
     res.send("Hello from the Dashboard!.")
 })
-
 
 app.use('/test',(req,res)=>{
     res.send("Hello from the server!.")
@@ -15,3 +27,5 @@ app.use('/test',(req,res)=>{
 app.listen(7777,()=>{
     console.log("Server is successfully listening on port 7777...")
 })
+
+
