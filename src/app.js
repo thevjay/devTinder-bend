adafcad86668f668acdd03615859f052a55ac272
Vijay <rebalvijay930@gmail.com>
2025-03-14 21:47:59 +0530

    multiple-route handlers

--- src/app.js
+++ src/app.js
@@ -2,19 +2,25 @@ const express = require('express')
 
 const app = express();
 
-// ab?c --> /ac, /abc
-// a(bc)?d  -->  /ad  /abcd
-//Regx /a/ ->> /a^ 
-// /.*fly/ --> butterfly
 
-// Daynamic Routing
-
-app.get("/user/:userId/:name/:password",(req,res)=>{
-    console.log(req.params)
-    //console.log(req.query)
-    res.send({firstName: "Vijya",lastName:"Saini"})
+app.use("/user",
+    [(req,res,next)=>{
+    // Route Handler
+    // res.send("Route Hanlder 1");
+    console.log("Handling the route user 1!!");
+    // res.send("1st Response")
+    next();
+    //res.send("1st Response")
+}],[(req,res,next)=>{
+    //Route Handler 2
+    console.log("Handling the route user 2!!");
+    //res.send("2nd Response");
+    next();
+}],(req,res)=>{
+    //Route Handler 2
+    console.log("Handling the route user 3!!");
+    res.send("3nd Response");
 })
-
 
 app.listen(7777,()=>{
     console.log("Server is successfully listening on port 7777...")
