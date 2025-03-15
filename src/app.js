a997c539134e58b0f222d58410ff4c3d16a088aa
Vijay <rebalvijay930@gmail.com>
2025-03-15 11:29:58 +0530

    get APIs

--- src/app.js
+++ src/app.js
@@ -21,14 +21,48 @@ app.post("/signup",async(req,res)=>{
     }
 })
 
-connectDB().then(()=>{
+// Get user by email
+app.get("/user",async(req,res)=>{
+    const userEmail = req.body.emailId;
+
+    try{
+       const user = await User.findOne({ emailId: userEmail });
+       if (user.length === 0){
+        res.status(404).send("User not found")
+       }else{
+        res.send(user);
+       }
+
+    //    res.send(user);
+    }
+    catch(err){
+        res.status(500).send("Something went wrong ")
+    }
+    
+})
+
+// Feed API - GET /feed - get all the users from the database
+app.get('/feed',async(req,res)=>{
+
+    try{
+        const users = await User.find({});
+
+        res.send(users)
+    }
+    catch(err){
+        res.status(500).send("Something went wrong ")
+    }
+})
+
+connectDB()
+    .then(()=>{
     console.log("Database connection established...");
     app.listen(7777,()=>{
         console.log("Server is successfully listening on port 7777...")
     });    
-})
-.catch((err)=>{
+    })
+    .catch((err)=>{
     console.error("Database cannot be connected"+err)
-})
+    })
 
 
