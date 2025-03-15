e51cca7c2acdd46e173d87a9356659b599431675
Vijay <rebalvijay930@gmail.com>
2025-03-15 12:48:42 +0530

    schema validation

--- src/app.js
+++ src/app.js
@@ -54,6 +54,35 @@ app.get('/feed',async(req,res)=>{
     }
 })
 
+app.delete("/user",async(req,res)=>{
+    try{
+        const userId = req.body.userId;
+        const user = await User.findByIdAndDelete(userId);
+
+        res.send("User Deleted Successfully")
+    }
+    catch(err){
+        res.status(500).send("Something went wrong ")
+    }
+})
+
+// Update data of the user
+app.patch("/user",async(req,res)=>{
+    try{
+        const userId = req.body.userId;
+        const data = req.body;
+
+        await User.findByIdAndUpdate({_id:userId},data,{
+            returnDocument:"after",runValidators:true
+        });
+
+        res.send("User updated successfully")
+    }
+    catch(err){
+        res.status(500).send("Something went wrong ")
+    }
+})
+
 connectDB()
     .then(()=>{
     console.log("Database connection established...");
--- src/models/user.js
+++ src/models/user.js
@@ -3,23 +3,48 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     firstName:{
         type:String,
+        required:true,
+        miniLength:4,
+        maxLength:50,
     },
     lastName:{
         type:String,
     },
     emailId:{
         type:String,
+        required:true,
+        lowercase:true,
+        unique:true,
+        trim:true,
     },
     password:{
         type:String,
+        required:true,
     },
     age:{
         type:Number,
+        min:18,
     },
     gender:{
         type:String,
+        validate(value){
+            if(!["male","female","others"].includes(value)){
+                throw new Error("Gender data is not valid")
+            }
+        }
+    },
+    photoUrl:{
+        type:String,
+        default:"https://www.tenforums.com/attachments/tutorial-test/146359d1501443008-change-default-account-picture-windows-10-a-user.png"
+    },
+    about:{
+        type:String,
+        default:"This is a default User"
+    },
+    skills:{
+        type:[String],
     }
-})
+},{timestamps:true})
 
 const User = mongoose.model("User",userSchema);
 module.exports = User;
